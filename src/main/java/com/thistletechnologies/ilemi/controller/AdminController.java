6e78dc9e987224c125d32541cafbe04e7cc43bc1
Ebelechukwu Doris Okolo <dorisebele47@gmail.com>
2023-11-15 15:04:23 +0100

    commiting userService methods

---
+++ src/main/java/com/thistletechnologies/ilemi/controller/AdminController.java
@@ -0,0 +1,39 @@
+package com.thistletechnologies.ilemi.controller;
+
+import com.thistletechnologies.ilemi.dto.request.JoinWaitingListRequest;
+import com.thistletechnologies.ilemi.dto.response.JoinWaitingListResponse;
+import com.thistletechnologies.ilemi.model.User;
+import com.thistletechnologies.ilemi.serviceImplementation.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+        private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/get-user-by-id/{userId}")
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable ("userId") String userId) {
+        return new ResponseEntity<>(userService.getUserById(userId), HttpStatus.OK);
+
+    }
+    @GetMapping("/get-all-users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
+    }
+    @GetMapping("/get-user-by-email/{email}")
+    public ResponseEntity<Optional<User>> getUserByEmail(@PathVariable ("email") String email){
+        return new ResponseEntity<>(userService.getUserByEmail(email), HttpStatus.OK);
+    }
+
+    }
---
+++ src/main/java/com/thistletechnologies/ilemi/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.thistletechnologies.ilemi.controller;
+
+import com.thistletechnologies.ilemi.dto.request.JoinWaitingListRequest;
+import com.thistletechnologies.ilemi.dto.response.JoinWaitingListResponse;
+import com.thistletechnologies.ilemi.serviceImplementation.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/join-wait-list")
+    public ResponseEntity<JoinWaitingListResponse> createProduct(@RequestBody JoinWaitingListRequest joinWaitingListRequest){
+        return new ResponseEntity<>(userService.joinWaitingList(joinWaitingListRequest), HttpStatus.CREATED);
+    }
+
+}
